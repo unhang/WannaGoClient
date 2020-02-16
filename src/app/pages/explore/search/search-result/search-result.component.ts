7f14f05d64ebca952008ced2883a42471d80858a
unhang11@gmail.com <unhang11@gmail.com>
2020-02-16 09:40:29 +0700

    16/2

--- src/app/pages/explore/search/search-result/search-result.component.ts
+++ src/app/pages/explore/search/search-result/search-result.component.ts
@@ -1,27 +1,28 @@
-import {AfterViewInit, Component, Input, OnInit} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {StayDetail, StayService} from 'src/swagger';
 import {MockDataService} from 'src/app/service/mock-data.service';
 
 @Component({
-  selector: 'app-search-result',
-  templateUrl: './search-result.component.html',
-  styleUrls: ['./search-result.component.scss'],
+    selector: 'app-search-result',
+    templateUrl: './search-result.component.html',
+    styleUrls: ['./search-result.component.scss'],
 })
-export class SearchResultComponent implements OnInit{
+export class SearchResultComponent implements OnInit {
 
-  stays: StayDetail [] = [];
+    stays: StayDetail [] = [];
 
-  constructor(
-      private stayService: StayService,
-      private mockDataService: MockDataService) { }
+    constructor(
+        private stayService: StayService,
+        private mockDataService: MockDataService) {
+    }
 
-  ngOnInit() {
-    console.log('hohooho');
-    setTimeout(() => {
-      this.stayService.search('01-05-2020', '31-05-2020', 79, 12, 1, 'vn')
-          .subscribe(res => {
-            this.stays = this.mockDataService.multiply<StayDetail>(res.result, 20);
-          });
-    }, 1000);
-  }
+    ngOnInit() {
+        console.log('hohooho');
+        setTimeout(() => {
+            this.stayService.search('01-05-2020', '31-05-2020', 79, 12, 1, 'vn')
+                .subscribe(res => {
+                    this.stays = this.mockDataService.multiply<StayDetail>(res.result, 20);
+                });
+        }, 1000);
+    }
 }
