74975d67ab16d96cbbb36488df2c3d0a0adbb83c
Mariohandsome <duythientran1998@gmail.com>
2020-02-22 10:59:36 +0700

    fix hight place

--- src/app/pages/explore/home/special-area/area-card/area-card.component.ts
+++ src/app/pages/explore/home/special-area/area-card/area-card.component.ts
@@ -8,10 +8,26 @@ import {HighlightPlace} from '../../../../../../swagger';
 })
 export class GoAreaCardComponent implements OnInit {
 
+  lang = localStorage.getItem('lang')
+  textVn: any = {
+    avgPrice: 'Giá trung bình',
+    addressPrice: 'chỗ ở',
+    unitPrice: 'VNĐ'
+  };
+  textEn: any = {
+    avgPrice: 'Average price',
+    addressPrice: 'place',
+    unitPrice: 'VND'
+  };
+
+  text: any = {};
+
   @Input() place: HighlightPlace
 
   constructor() { }
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.text = this.lang === 'en' ? this.textEn : this.textVn;
+  }
 
 }
