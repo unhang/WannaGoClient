941d5929c9328d05dfe8479227d3d8171d2cef4c
Mariohandsome <duythientran1998@gmail.com>
2020-03-01 16:22:57 +0700

    update login, register, lost-password

--- src/app/pages/profile/lost-pwd/form-lost-pwd/form-lost-pwd.component.ts
+++ src/app/pages/profile/lost-pwd/form-lost-pwd/form-lost-pwd.component.ts
@@ -6,8 +6,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./form-lost-pwd.component.scss'],
 })
 export class FormLostPwdComponent implements OnInit {
+  lang = localStorage.getItem('lang');
+  textVn: any = {
+    title: 'Quên mật khẩu',
+    plname: 'Họ và tên',
+    plemail: 'Email',
+    plphone: 'Số điện thoại',
+    plusername: 'Tên tài khoản',
+    plpassword: 'Nhập mật khẩu',
+    btnLogin: 'Gửi yêu cầu',
+    linkForget: 'Quên mật khẩu?',
+    linkRegister: 'Chưa có tài khoản?',
+  };
+  textEn: any = {
+    title: 'For get password',
+    plname: 'Full name',
+    plemail: 'Email',
+    plphone: 'Phone number',
+    plusername: 'Username',
+    plpassword: 'Your password',
+    btnLogin: 'Send submit',
+    linkForget: 'Quên mật khẩu?',
+    linkRegister: 'Chưa có tài khoản?',
+  };
+  text: any = {};
 
-  constructor() { }
+  constructor() {
+    this.text = this.lang === 'en' ? this.textEn : this.textVn;
+  }
 
   ngOnInit() {}
 
