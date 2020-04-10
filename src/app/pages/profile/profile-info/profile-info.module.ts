eecdc96c6d5f7a4be8cb054b7f0f9625816e010b
unhang <48117315+unhang@users.noreply.github.com>
2020-04-10 22:56:54 +0700

    Revert "Fixbug/no14"

--- src/app/pages/profile/profile-info/profile-info.module.ts
+++ src/app/pages/profile/profile-info/profile-info.module.ts
@@ -1,6 +1,6 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
-import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {FormsModule} from '@angular/forms';
 
 import {IonicModule} from '@ionic/angular';
 
@@ -15,8 +15,7 @@ import {ComponentModule} from '../../../components/component.module';
         FormsModule,
         IonicModule,
         ProfileInfoPageRoutingModule,
-        ComponentModule,
-        ReactiveFormsModule
+        ComponentModule
     ],
     declarations: [ProfileInfoPage]
 })
