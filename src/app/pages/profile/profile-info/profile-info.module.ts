24d16000ad107bac7b7270de156b9a42ba569295
Mariohandsome <duythientran1998@gmail.com>
2020-04-11 08:53:20 +0700

    update code before action  revert

--- src/app/pages/profile/profile-info/profile-info.module.ts
+++ src/app/pages/profile/profile-info/profile-info.module.ts
@@ -1,6 +1,6 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
-import {FormsModule} from '@angular/forms';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 
 import {IonicModule} from '@ionic/angular';
 
@@ -15,7 +15,8 @@ import {ComponentModule} from '../../../components/component.module';
         FormsModule,
         IonicModule,
         ProfileInfoPageRoutingModule,
-        ComponentModule
+        ComponentModule,
+        ReactiveFormsModule
     ],
     declarations: [ProfileInfoPage]
 })
